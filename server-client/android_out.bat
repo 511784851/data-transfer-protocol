26ee253b55f75511ed7864769d458792aefb8a53
yangjqun <160202556@qq.com>
2016-02-19 10:56:26 +0800

    修改生成android用protobuf的脚本，bat->perl

--- server-client/android_out.bat
+++
@@ -1,7 +0,0 @@
-protoc --java_out=../java login.proto
-protoc --java_out=../java result.proto
-protoc --java_out=../java main.proto
-protoc --java_out=../java social.proto
-protoc --java_out=../java chat.proto
-
-pause
