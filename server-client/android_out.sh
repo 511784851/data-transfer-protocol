44a07cae44482045a70fc727b7ba29f9fedc85a1
yangjqun <160202556@qq.com>
2016-03-09 14:49:12 +0800

    1.修改生成java文件的方式为javanano 2.remove android_out.sh, use java_out.pl instead

--- server-client/android_out.sh
+++
@@ -1,5 +0,0 @@
-../protoc --java_out=../generated-java login.proto
-../protoc --java_out=../generated-java result.proto
-#protoc --java_out=../java main.proto
-#protoc --java_out=../java social.proto
-#protoc --java_out=../java chat.proto
