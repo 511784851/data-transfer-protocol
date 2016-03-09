b57df8fd3e3badc8bd16f0a5fa05394efb3cb372
blemobi-jenkins <blemobi-jenkins@users.noreply.github.com>
2016-03-09 11:56:43 -0800

    Update android_out.sh

--- server-client/android_out.sh
+++ server-client/android_out.sh
@@ -1,5 +1,5 @@
-../protoc ----javanano_out=../generated-java login.proto
-../protoc ----javanano_out=../generated-java result.proto
+../protoc --javanano_out=../generated-java login.proto
+../protoc --javanano_out=../generated-java result.proto
 #protoc --java_out=../java main.proto
 #protoc --java_out=../java social.proto
 #protoc --java_out=../java chat.proto
