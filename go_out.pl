4abb3c4b3227ad3b62b06c31a44b607c547ee51f
yangjqun <160202556@qq.com>
2016-03-31 11:44:58 +0800

    把server-server/account_api.proto还是加入编译列表，不然go编译会报错：fileDescriptorXX redeclared in this block

--- go_out.pl
+++ go_out.pl
@@ -17,7 +17,7 @@ system($cmd);
 
 #################server_server protos#######################
 $server_server_protos = "server-server/login_api.proto ".
-                        "";
+                        "server-server/account_api.proto ";
 
 $cmd = "protoc -Iserver-server --go_out=../go-commons/service/ ".$server_server_protos;
 system($cmd);
--- server-server/go_out.pl
+++ server-server/go_out.pl
@@ -3,7 +3,7 @@
 use Cwd;
 $pwd = cwd();
 
-$cmd = "../protoc --go_out=../../go-commons/service/ login_api.proto";
+$cmd = "../protoc --go_out=../../go-commons/service/ login_api.proto account_api.proto";
 print("\n");
 system($cmd);
 system(pause);
