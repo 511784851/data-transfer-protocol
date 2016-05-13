eacf88b6d0ac1417610346b7e8873c23edd3b5b1
高圣易 <eason.gao@blemobi.com>
2016-05-14 02:47:04 +0800

    添加notification的自动编译protobuf项

--- go_out.pl
+++ go_out.pl
@@ -15,6 +15,7 @@ $server_client_protos = "server-client/result.proto ".
 						"server-client/oss.proto ".
 						"server-client/news.proto ".
 						"server-client/comment.proto ".
+						"server-client/notification.proto ".
 						"server-client/social.proto ".
 						"server-client/platform.proto";
 
