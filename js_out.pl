2c0a56c5cb4dcf068fe8930528c10d324d0123a0
heviMiwu <hevi.ye@blemobi.com>
2017-05-31 14:12:43 +0800

    js_out.pl

--- js_out.pl
+++ js_out.pl
@@ -9,17 +9,39 @@ if (@ARGV > 0) {
 }
 
 $protos = "server-client/result.proto ".
-		"server-client/account.proto ".
-		"server-client/login.proto ".
-		"server-client/oss.proto ".
-		"server-client/news.proto ".
-		"server-client/comment.proto ".
-		"server-client/platform.proto ".
-		"server-client/datapublishing.proto ".
-		"server-client/community.proto ".
-		"server-client/fans.proto ".
-		"server-client/netdisk.proto ".
-		"server-server/news_inside.proto ";
+                        "server-client/account.proto ".
+						"server-client/login.proto ".
+						"server-client/oss.proto ".
+						"server-client/news.proto ".
+						"server-client/comment.proto ".
+						"server-client/social.proto ".
+						"server-client/platform.proto ".
+						"server-client/datapublishing.proto ".
+						"server-client/notification.proto ".
+						"server-client/community.proto ".
+						"server-client/contact.proto ".
+						"server-client/netdisk.proto ".
+						"server-client/streaming.proto ".
+						"server-client/chat.proto ".
+						"server-client/task.proto ".
+						"server-client/edge.proto ".
+						"server-client/achievement.proto ".
+						"server-client/fans.proto ".
+						"server-server/common_inside.proto ".
+						"server-server/login_inside.proto ".
+                        "server-server/account_inside.proto ".
+						"server-server/notification_inside.proto ".
+						"server-server/datapublishing_inside.proto ".
+						"server-server/news_inside.proto ".
+						"server-server/achievement_inside.proto ".
+						"server-server/community_inside.proto ".
+						"server-server/comment_inside.proto ".
+						"server-server/oss_inside.proto ".
+						"server-server/platform_inside.proto ".
+						"server-client/robot.proto ".
+						"server-server/robot_inside.proto ".
+						"server-server/social_inside.proto ".
+						"server-server/netdisk_inside.proto";
 
 
 $cmd = "protoc -Iserver-client -Iserver-server --js_out=import_style=commonjs,binary:${output} ${protos}";
